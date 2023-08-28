78dbc35218cc221b6761a54f62be48c340b5df1a
hafiz <hafizcoolman@gmail.com>
2023-08-28 17:40:11 +0800

    [Task] update listing with completeTask function

--- src/views/Tasks/Listing.vue
+++ src/views/Tasks/Listing.vue
@@ -17,18 +17,25 @@
               <th>Name</th>
               <th>Status</th>
               <th>Created At</th>
-              <th>Action</th>
+              <th class="text-center">Action</th>
             </tr>
           </thead>
           <tbody v-if="tasks.length > 0">
             <tr v-for="(task, index) in tasks" :key="index">
-              <td>{{ task.id }}</td>
+              <td>{{ index+1 }}</td>
               <td>{{ task.name }}</td>
               <td>{{ task.is_completed }}</td>
               <td>{{ task.created_at }}</td>
               <td>
-                <RouterLink :to="{ path: '/tasks/' + task.id + '/edit' }" class="btn btn-info"> Edit </RouterLink>
-                <button type="button" class="btn btn-danger">Delete</button>
+                <div class="d-flex justify-content-center">
+                  <RouterLink :to="{ path: '/tasks/' + task.id + '/edit' }" class="btn btn-info min-width-5em ms-3"> Edit </RouterLink>
+                  <button v-show="!task.is_completed" type="button" class="btn btn-success min-width-5em ms-3" @click="completeTask(task.id)">
+                    Complete
+                  </button>
+                  <button v-show="task.is_completed" disabled type="button" class="btn btn-secondary min-width-5em ms-3" @click="completeTask(task.id)">
+                    Completed
+                  </button>
+                </div>
               </td>
             </tr>
           </tbody>
@@ -51,6 +58,7 @@ export default {
   data() {
     return {
       tasks: [],
+      is_completed: true,
     };
   },
   mounted() {
@@ -64,6 +72,22 @@ export default {
         let data = res.data;
         this.tasks = data.data;
         console.log(this.tasks);
+      });
+    },
+
+    completeTask(task_id) {
+
+      let formData = new FormData();
+      formData.append('_method', 'PATCH');
+      formData.append('is_completed', 1 );
+
+      axios
+      .post("http://laravel-10-api.test/api/v1/tasks/"+task_id+"/complete", formData)
+      .then((res) => {
+        this.getTasks();
+      })
+      .catch((err) => {
+        console.log(err);
       });
     },
   },
